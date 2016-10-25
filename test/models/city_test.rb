9c114e86b9b1a875199ed8aecf3305504789590b
vicky142713 <vicky142713@gmail.com>
2016-10-25 14:47:14 -0400

    forecast

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -1,25 +1,20 @@
 require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
-  setup do
-    @city = cities(:one)
-    @cityWrong = cities(:two)
-  end
-  
-  test "should create city" do
-    VCR.use_cassette("nominatim") do
-      assert_difference('City.count') do
-        city = City.new  name: @city.name
-        city.save
+ setup do
+   @city = cities(:one)
+   @cityWrong = cities(:two)
+ end
+ 
+ test "should create city" do
+   VCR.use_cassette("nominatim") do
+   assert_difference('City.count') do
+     city = City.new  name: @city.name
+     city.save
       end
-    end
-  end
-  
-  test "should break on unknown city" do
-    VCR.use_cassette("nominatim2") do
-        city = City.new  name: @cityWrong.name
-        city.save
-        assert city.invalid?
-    end
-  end
+     end
+   end
+
+
+   
 end
