283314f358fd9c2ad6709dea86e65825c2ac3e01
vicky142713 <vicky142713@gmail.com>
2016-10-27 14:12:52 -0400

    vicky

---
+++ public/angular-seed/app/view1/view1.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+    $scope.students = [];
+
+    $http.get('/students.json').success(function(data){
+      $scope.students = data
+  	});
+
+}]);
