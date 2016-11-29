f2d83fc11b5d5ec4a3fa8119dfdce990b7c45600
vicky142713 <vicky142713@gmail.com>
2016-11-29 03:15:17 -0500

    pp

--- test/models/city_test.rb
+++ test/models/city_test.rb
@@ -1,19 +1,15 @@
+
 require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
- test "invalide city" do
-   VCR.use_cassette("nominatim2") do
-     incorrect = City.new name: cities(two:).name
-     assert_incorrect.invalide?
-   end
- end
   
- test "valide city" do
-   VCR.use_cassette("nominatim1") do
-     incorrect = City.new name: cities(one:).name
-     assert_incorrect.valide?
-   end
- end
+  test "valid city" do
+    VCR.use_cassette("nominatim1") do
+      incorrect = City.new name: cities(:one).name
+      assert incorrect.valid?
+    end
+  end  
   
+
 
 end
